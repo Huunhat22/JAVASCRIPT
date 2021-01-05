541919c31cc923cc42eabdc69037e4b9d8d5cfb8
HuuNhat <huunhatth07@gmai.com>
2021-01-05 17:42:36 +0700

    update thêm cách sử dụng hàm addEventListener

--- DOM_part2.js
+++ DOM_part2.js
@@ -1,1 +1,23 @@
 //Thực hành DOM part2 với dropdown list
+var dropdownlist = Array.from(document.querySelectorAll('.dropdown'));
+
+dropdownlist.forEach(
+    function(elemnt,index){
+        var btn = elemnt.querySelector('.btn');
+        // console.log(btn);
+        // sử dụng hàm thêm xử lý sự kiện
+        btn.addEventListener('click',function(event) {
+            // console.log(this);
+            if (elemnt.classList.value.indexOf(' drop') === -1) {
+                elemnt.classList.add('drop');
+            } else {
+                elemnt.classList.remove('drop');
+            }
+            
+            /*
+                nếu phần tử element đã tồn tại class drop thì remvove
+                nếu chưa có thì thêm mới class -> drop
+            */
+
+        });
+})
