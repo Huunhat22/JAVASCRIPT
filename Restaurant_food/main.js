338c49e246f39a115e24c520b45d17a15565ff4b
HuuNhat <huunhatth07@gmai.com>
2021-01-22 16:51:55 +0700

    Add new script for web - show menu

---
+++ Restaurant_food/main.js
@@ -0,0 +1,9 @@
+/* ======================== SHOW MENU  ===========================*/
+const showMenu = (toggleId,navId) =>{
+    const toggle = document.getElementById(toggleId),
+        navmenu = document.getElementById(navId)
+
+        if (condition) {
+            
+        }
+}
