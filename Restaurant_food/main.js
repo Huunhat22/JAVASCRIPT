119643622da96f7e7941ce48f2c1ebc26a12f19b
huunhat22 <huunhatth07@gmail.com>
2021-01-24 23:56:59 +0700

    Thêm xử lý sự kiện click vào menu.

--- Restaurant_food/main.js
+++ Restaurant_food/main.js
@@ -1,9 +1,24 @@
 /* ======================== SHOW MENU  ===========================*/
-const showMenu = (toggleId,navId) =>{
+// debugger
+const showMenu = (toggleId, navId) => {
     const toggle = document.getElementById(toggleId),
         navmenu = document.getElementById(navId)
+    // kiểm tra khác null thì thực hiện
+    if (navmenu && toggle) {
+        toggle.addEventListener('click' , ()=>{
+            navmenu.classList.toggle('header__menu-show');
+        })
+    }
 
-        if (condition) {
-            
+    /* cách 2 sử dụng hàm add và remove */
+    /* toggle.addEventListener('click', () => {
+        if (navmenu.classList.value.indexOf('header__menu-show') === -1) {
+            navmenu.classList.add('header__menu-show');
         }
+        else {
+            navmenu.classList.remove('header__menu-show');
+        }
+    }) */
+
 }
+showMenu('header-toggle', 'nav-menu');
