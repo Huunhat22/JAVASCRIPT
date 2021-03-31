5321f08d6bbf2117d160b9b212e5741a1a343a52
huunhat22 <huunhatth07@gmail.com>
2021-03-31 23:34:30 +0700

    Validation form sign up

---
+++ Validation_Form/Script_validation.js
@@ -0,0 +1,21 @@
+
+//định nghĩa đối tượng
+function Validator(options){
+    var formElement = document.querySelector(options.form);
+    // console.log(formElement);
+    if (formElement) {
+        
+    } else {
+        
+    }
+}
+
+
+//định nghĩa rules
+Validator.isRequired = function(){
+
+}
+
+Validator.isEmail = function(){
+
+}
