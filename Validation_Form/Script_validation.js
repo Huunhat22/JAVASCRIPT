976662817673209cbd4f0239d8defa90a6013297
HuuNhat <huunhatth07@gmai.com>
2021-04-01 17:40:10 +0700

    update

--- Validation_Form/Script_validation.js
+++ Validation_Form/Script_validation.js
@@ -4,18 +4,53 @@ function Validator(options){
     var formElement = document.querySelector(options.form);
     // console.log(formElement);
     if (formElement) {
-        
-    } else {
-        
+        options.rules.forEach(rule => {
+            // console.log(rule.selector);
+            /*
+                phải truy xuất element từ thẻ cha gần nhất, tránh việc truy xuất từ document 
+            */
+            var inputElement = formElement.querySelector(rule.selector);
+            var errorElement = inputElement.parentElement.querySelector('.Signup__form-message'); 
+            // console.log(inputElement);
+            if (inputElement) {
+                inputElement.addEventListener('blur', function () {
+                
+
+                    var valueInput = rule.test(inputElement.value);
+
+                    if (valueInput) {
+                        errorElement.innerText = valueInput;
+                    } else {
+                        errorElement.innerText = '';
+                    }
+                })
+            }
+        });
     }
 }
 
 
 //định nghĩa rules
-Validator.isRequired = function(){
-
+/*
+    1: khi có lỗi thì trả ra message lỗi
+    2: khi hợp lệ -> không trả ra cái gì hết. (undefind)
+*/
+Validator.isRequired = function (selector){
+    // return về 1 object
+    // Key : value
+    return {
+        selector : selector,
+        test: function (value){
+            return value.trim() ? undefined : 'Vui lòng nhập trường này'
+        }
+    }
 }
 
-Validator.isEmail = function(){
+Validator.isEmail = function (selector){
+     return {
+         selector : selector,
+         test : function (){
 
+         }
+     }
 }
