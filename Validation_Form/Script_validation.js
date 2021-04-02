95fe667a96b597aaa38fb70e644d6b2cc210ce15
HuuNhat <huunhatth07@gmai.com>
2021-04-02 17:33:41 +0700

    update validate for password

--- Validation_Form/Script_validation.js
+++ Validation_Form/Script_validation.js
@@ -1,8 +1,24 @@
 
 //định nghĩa đối tượng
 function Validator(options){
+
+    // Hàm báo xử lý xuất hiện thông báo lỗi
+    function validate(inputElement,rule) {
+        var errorMessage = rule.test(inputElement.value);         /* Value ở đây là properties của thẻ input, 
+                                                                      errorMessage ở đây là giá trị trả về của hàm test()      */
+        var errorElement = inputElement.parentElement.querySelector(options.errorSelector); 
+
+        if (errorMessage) {
+            errorElement.innerText = errorMessage;
+            inputElement.classList.add('validate');
+        } else {
+            errorElement.innerText = '';
+            inputElement.classList.remove('validate');
+        }
+    }
+
+    // Lấy Element của form cần validate
     var formElement = document.querySelector(options.form);
-    // console.log(formElement);
     if (formElement) {
         options.rules.forEach(rule => {
             // console.log(rule.selector);
@@ -10,19 +26,21 @@ function Validator(options){
                 phải truy xuất element từ thẻ cha gần nhất, tránh việc truy xuất từ document 
             */
             var inputElement = formElement.querySelector(rule.selector);
-            var errorElement = inputElement.parentElement.querySelector('.Signup__form-message'); 
+            
             // console.log(inputElement);
+            // Xử lý trường hợp blur ra khỏi input
             if (inputElement) {
                 inputElement.addEventListener('blur', function () {
-                
+                    validate(inputElement,rule);
+                })
+            }
 
-                    var valueInput = rule.test(inputElement.value);
-
-                    if (valueInput) {
-                        errorElement.innerText = valueInput;
-                    } else {
-                        errorElement.innerText = '';
-                    }
+            // Xử lý trường hợp bắt lỗi Email
+            if (inputElement) {
+                inputElement.addEventListener('input', function () {
+                    var errorElement = inputElement.parentElement.querySelector(options.errorSelector); 
+                    errorElement.innerText = '';
+                    inputElement.classList.remove('validate');
                 })
             }
         });
@@ -40,7 +58,7 @@ Validator.isRequired = function (selector){
     // Key : value
     return {
         selector : selector,
-        test: function (value){
+        test: function (value){           /* value ở đây là giá trị người dùng nhập vào input */
             return value.trim() ? undefined : 'Vui lòng nhập trường này'
         }
     }
@@ -49,8 +67,18 @@ Validator.isRequired = function (selector){
 Validator.isEmail = function (selector){
      return {
          selector : selector,
-         test : function (){
+         test : function (value){
+             var regex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+             return regex.test(value) ? undefined : 'Trường này phải là Email'
+         }
+     }
+}
 
+Validator.minlenght = function (selector,min){
+     return {
+         selector : selector,
+         test : function (value){
+             return value.length >= min ? undefined : `Vui lòng nhập lớn hơn ${min} kí tự`
          }
      }
 }
