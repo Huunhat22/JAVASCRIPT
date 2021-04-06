31c5d0f72cee86c7457767a01808bd88a2b97650
huunhat22 <huunhatth07@gmail.com>
2021-04-06 23:59:15 +0700

    thêm xử lý confirm cho password và xử lý xuất ra nhiều thông báo lỗi cho 1 input.

--- Validation_Form/Script_validation.js
+++ Validation_Form/Script_validation.js
@@ -2,11 +2,28 @@
 //định nghĩa đối tượng
 function Validator(options){
 
+    // khai báo 1 hàm là 1 object rỗng -> sử dụng để 1 input áp dụng được nhiều rules
+    var selectorRules = {};
+
     // Hàm báo xử lý xuất hiện thông báo lỗi
     function validate(inputElement,rule) {
-        var errorMessage = rule.test(inputElement.value);         /* Value ở đây là properties của thẻ input, 
-                                                                      errorMessage ở đây là giá trị trả về của hàm test()      */
+        /*  var errorMessage = rule.test(inputElement.value); */         /* Value ở đây là properties của thẻ input, 
+                                                                         errorMessage ở đây là giá trị trả về của hàm test()      */
         var errorElement = inputElement.parentElement.querySelector(options.errorSelector); 
+        var errorMessage;
+        
+        
+        // Lấy ra các rules của các selector
+        var rules = selectorRules[rule.selector];
+       
+        // Lặp qua từng rule và kiểm tra
+        for (let i = 0; i < rules.length; i++) {
+            
+            errorMessage = rules[i](inputElement.value);
+
+            // Nếu mà có lỗi thì dừng kiểm tra và thoát.
+            if(errorMessage) break;
+        }
 
         if (errorMessage) {
             errorElement.innerText = errorMessage;
@@ -22,6 +39,15 @@ function Validator(options){
     if (formElement) {
         options.rules.forEach(rule => {
             // console.log(rule.selector);
+
+            /* Lưu lại các rules cho mỗi input */
+            // selectorRules[rule.selector] = rule.test;
+            if (Array.isArray(selectorRules[rule.selector])) {
+                selectorRules[rule.selector].push(rule.test);
+            } else {
+                selectorRules[rule.selector] = [rule.test];
+            }
+
             /*
                 phải truy xuất element từ thẻ cha gần nhất, tránh việc truy xuất từ document 
             */
@@ -44,6 +70,8 @@ function Validator(options){
                 })
             }
         });
+
+        // console.log(selectorRules);
     }
 }
 
@@ -53,13 +81,13 @@ function Validator(options){
     1: khi có lỗi thì trả ra message lỗi
     2: khi hợp lệ -> không trả ra cái gì hết. (undefind)
 */
-Validator.isRequired = function (selector){
+Validator.isRequired = function (selector,message){
     // return về 1 object
     // Key : value
     return {
         selector : selector,
         test: function (value){           /* value ở đây là giá trị người dùng nhập vào input */
-            return value.trim() ? undefined : 'Vui lòng nhập trường này'
+            return value.trim() ? undefined : message || 'Vui lòng nhập trường này'
         }
     }
 }
@@ -74,11 +102,20 @@ Validator.isEmail = function (selector){
      }
 }
 
-Validator.minlenght = function (selector,min){
+Validator.minlenght = function (selector,min,message){
      return {
          selector : selector,
          test : function (value){
              return value.length >= min ? undefined : `Vui lòng nhập lớn hơn ${min} kí tự`
          }
      }
+}
+
+Validator.isConfirmed = function(selector,getConfirmValue,message){         /* getConfirmValue là 1 callback function , nên khi return về thì phải thêm ()  / tham số thứ 3 message là thông báo lỗi customize    */
+    return{
+        selector : selector,
+        test : function(value){
+            return value === getConfirmValue() ? undefined : message || 'Giá trị nhập không đúng'
+        }
+    }
 }
